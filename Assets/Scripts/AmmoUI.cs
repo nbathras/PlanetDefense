e113f0769312a000b98eacbba7b9ee2e2080483f
Noah Bathras <nbathras@terpmail.umd.edu>
2020-12-31 14:30:45 -0500

    Added Ammo UI

---
+++ Assets/Scripts/AmmoUI.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class AmmoUI : MonoBehaviour {
+
+    [SerializeField] private Transform ammoUIWorldTransform;
+
+    [SerializeField] private TextMeshProUGUI ammoText;
+
+    private Camera mainCamera;
+
+    private void Awake() {
+        mainCamera = Camera.main;
+    }
+
+    private void Start() {
+        CannonController.Instance.OnAmmoAmountChangedEvent += AmmoUI_OnAmmoAmountChangedEvent;
+    }
+
+    private void Update() {
+        Vector3 ammoUIScreenPosition = mainCamera.WorldToScreenPoint(ammoUIWorldTransform.position) + (new Vector3(0, -10, 0));
+        ammoText.transform.position = ammoUIScreenPosition;
+    }
+
+    private void AmmoUI_OnAmmoAmountChangedEvent(object sender, System.EventArgs e) {
+        ammoText.SetText(CannonController.Instance.GetAmmo().ToString());
+    }
+}
--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -1,14 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CannonController : MonoBehaviour {
 
+    public static CannonController Instance;
+
+    public event EventHandler OnAmmoAmountChangedEvent;
+
     [SerializeField] private Transform projectileSpawnPosition;
 
+    [SerializeField] private int ammoMax;
+    private int ammo;
+
     private Camera mainCamera;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+    }
 
     private void Start() {
         // Cached main camera
         mainCamera = Camera.main;
+
+        ammo = ammoMax;
+        OnAmmoAmountChangedEvent?.Invoke(this, EventArgs.Empty);
     }
     
     private void Update() {
@@ -16,11 +33,25 @@ public class CannonController : MonoBehaviour {
 
         Vector3 aimDirection = (mousePosition - transform.position).normalized;
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-        transform.eulerAngles = new Vector3(0, 0, angle - 90);
 
-        // Fire Cannonball
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            Projectile.Create(projectileSpawnPosition.position, mousePosition);
+        if (angle > 2 && angle < 178) {
+            transform.eulerAngles = new Vector3(0, 0, angle - 90);
+
+            // Fire Cannonball
+            if (Input.GetKeyDown(KeyCode.Mouse0)) {
+                ReduceAmmountCount(1);
+                Projectile.Create(projectileSpawnPosition.position, mousePosition);
+            }
         }
     }
-}
+
+    private void ReduceAmmountCount(int a) {
+        ammo -= a;
+
+        OnAmmoAmountChangedEvent?.Invoke(this, EventArgs.Empty);
+    }
+
+    public int GetAmmo() {
+        return ammo;
+    }
+} 
