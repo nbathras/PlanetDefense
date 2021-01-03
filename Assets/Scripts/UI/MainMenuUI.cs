225a9fd02aad851d32ac3303688d139516870408
Noah Bathras <nbathras@terpmail.umd.edu>
2021-01-02 19:44:30 -0500

    Added scores to main menu

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using TMPro;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,14 +7,29 @@ public class MainMenuUI : MonoBehaviour {
 
     [Header("Transform Holder")]
     [SerializeField] private Transform mainMenuHolder;
+    [SerializeField] private Transform page1Holder;
+    [SerializeField] private Transform page2Holder;
 
     [Header("Buttons")]
     [SerializeField] private Button newGameButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private Button scoreButton;
+    [SerializeField] private Button backButton;
+
+    [Header("Text Fields")]
+    [SerializeField] private TextMeshProUGUI rankedListText;
+    [SerializeField] private TextMeshProUGUI scoreListText;
+    [SerializeField] private TextMeshProUGUI nameListText;
 
     private void Awake() {
         mainMenuHolder.gameObject.SetActive(true);
+        page1Holder.gameObject.SetActive(true);
+        page2Holder.gameObject.SetActive(false);
 
         newGameButton.onClick.AddListener(delegate { GameManager.Instance.SetUpGame(); });
+        exitButton.onClick.AddListener(delegate { Application.Quit(); });
+        scoreButton.onClick.AddListener(delegate { DisplayScores(); });
+        backButton.onClick.AddListener(delegate { DisplayManMenu(); });
     }
 
     private void Start() {
@@ -27,5 +43,15 @@ public class MainMenuUI : MonoBehaviour {
 
     private void MainMenuUI_OnGameSetupEvent(object sender, EventArgs e) {
         mainMenuHolder.gameObject.SetActive(false);
+    }
+
+    private void DisplayScores() {
+        page1Holder.gameObject.SetActive(false);
+        page2Holder.gameObject.SetActive(true);
+    }
+
+    private void DisplayManMenu() {
+        page1Holder.gameObject.SetActive(true);
+        page2Holder.gameObject.SetActive(false);
     }
 }
